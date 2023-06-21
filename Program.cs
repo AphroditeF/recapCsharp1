cb9091b363246bb82d181b85e7938dda7f1abb44
AphroditeF <a.flora@outlook.com>
2023-06-21 03:28:40 +0300

    Recap C# 1

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Denis!");
+            Console.WriteLine("Hello World!");
+            //Console.Read();
+
+            /*  Dictionary<string, decimal> itemPrices = new Dictionary<string, decimal>()
+              {
+                  {"cheese", 5.99 }
+              };*/
+
+            Dictionary<string, string> itemPrices = new Dictionary<string, string>()
+            {
+                {"cheese", "5.99" },
+                {"carrots","2.99" }
+            };
+            Console.WriteLine(itemPrices["cheese"]);
+            Console.WriteLine(itemPrices["carrots"]);
+
+            Dictionary<string, decimal> itemPrices2 = new Dictionary<string, decimal>()
+            {
+                {"cheese", 5.99m },
+                {"carrots",2.99m }
+            };
+            Console.WriteLine(itemPrices["cheese"]);
+            Console.WriteLine(itemPrices["carrots"]);
+
+
+        }
+    }
+}
+
